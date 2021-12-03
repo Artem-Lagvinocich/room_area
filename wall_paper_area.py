1faec871cf489e2cd02712c8b7c96af4fa0aab5e
Artem-Lagvinocich <lagvinovicharteme@gmail.com>
2021-12-03 13:51:55 +0300

    makeing abstractmethod

--- wall_paper_area.py
+++ wall_paper_area.py
@@ -1,7 +1,19 @@
-class Parameters:
+from abc import ABC, abstractmethod
+
+
+class Parameters(ABC):
     def __init__(self, l, w):
         self.length = l
         self.width = w
+
+    @abstractmethod
+    def area_to_wall_paper(self, r1, w1, d1):
+        area = r1.wall_area() - w1.window_area() - d1.door_area()
+        count_rolls = area // wp.get_paper_area()
+        part_of_the_roll = area % wp.get_paper_area()
+        print('Конечная площать для поклейки: ', area)
+        print('Кол-во целлых руллонов для поклейки: ', count_rolls)
+        print('Часть целого руллона: ', part_of_the_roll)
 
 
 class Room(Parameters):
@@ -9,6 +21,7 @@ class Room(Parameters):
         super().__init__(l, w)
         self.height = h
 
+    @abstractmethod
     def wall_area(self):
         wall_1 = self.length * self.height
         wall_2 = self.width * self.height
@@ -17,28 +30,21 @@ class Room(Parameters):
 
 
 class Window(Parameters):
+    @abstractmethod
     def window_area(self):
         return self.width * self.length
 
 
 class Door(Parameters):
+    @abstractmethod
     def door_area(self):
         return self.length * self.width
 
 
 class Wallpaper(Parameters):
+    @abstractmethod
     def get_paper_area(self):
         return self.length * self.width
-
-
-def area_to_wall_paper():
-    area = r1.wall_area() - w1.window_area() - d1.door_area()
-    count_rolls = area // wp.get_paper_area()
-    part_of_the_roll = area % wp.get_paper_area()
-    print('Конечная площать для поклейки: ', area)
-    print('Кол-во целлых руллонов для поклейки: ', count_rolls)
-    print('Часть целого руллона: ', part_of_the_roll)
-    return area, count_rolls, part_of_the_roll
 
 
 r1 = Room(6, 3, 2.7)
@@ -53,4 +59,4 @@ print('Door area: ', d1.door_area())
 wp = Wallpaper(10, 1)
 print('Wall paper area: ', wp.get_paper_area())
 
-area_to_wall_paper()
+# area_to_wall_paper()
