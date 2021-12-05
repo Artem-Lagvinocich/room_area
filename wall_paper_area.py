981736eb3c01c2280d9ad4eb3f8e7362fe47051d
Artyom <65786691+Artem-Lagvinocich@users.noreply.github.com>
2021-12-05 14:31:05 +0300

    Update wall_paper_area.py
    fix previous bad commit

--- wall_paper_area.py
+++ wall_paper_area.py
@@ -33,33 +33,6 @@ class Counting(Parameters):
         return self.width * self.length
 
 
-<<<<<<< HEAD
-room_parameters = Room(6, 3, 2.7)
-print('Room walls area: ', room_parameters.wall_area())
-=======
-class Door(Parameters):
-    def door_area(self):
-        return self.length * self.width
-
-
-class Wallpaper(Parameters):
-    def get_paper_area(self):
-        return self.length * self.width
-
-
-def area_to_wall_paper():
-    area = r1.wall_area() - w1.window_area() - d1.door_area()
-    count_rolls = area // wp.get_paper_area()
-    part_of_the_roll = area % wp.get_paper_area()
-    print('Конечная площать для поклейки: ', area)
-    print('Кол-во целлых руллонов для поклейки: ', count_rolls)
-    print('Часть целого руллона: ', part_of_the_roll)
-
-
-r1 = Room(6, 3, 2.7)
-print('Room walls area: ', r1.wall_area())
->>>>>>> c504cf6f0aeb6d27df3313242689a091776a11c4
-
 window_parameters = Counting(1.5, 2)
 print('Window area: ', window_parameters.get_area())
 
